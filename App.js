a6f7c426ccb9734e82fbb4d89dc0fa5d9f929f64
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-03-25 16:15:50 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"; 
+import { View, Text, TextInput, Button, StyleSheet} from "react-native"; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>No que você é grato hoje?</Text>
+            <TextInput style={styles.input}placeholder="Escreve algo pelo qual é grato..." />
+            <Button title="Compartilhar Inspiração" style={styles.button} />
+            <TouchableOpacity style={styles.button} onPress={() => console.log('Enviar mensagem')}>
+              <Text style={styles.buttonText}>Salvar Momento</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FCFFC1",
+    },
+    title: {
+        fontSize: 30,
+        color: "#FB9EC6",
+    },
+    button: {
+        backgroundColor: "blue",
+        borderRadius: 5,
+        width: 100,
+        color: "white",
+        fontSize: 15,
+    },
+    input: {
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        width: '100%',
+        height: 50,
+        fontSize: 15,
+        color: "grey",
+        marginTop: 10,
+        marginBottom: 20,
+        paddingLeft: 10,
+    },
 });
